0328b0fac5ada8e254cc26d17450c21209e83be1
10038_jatinoco_L00406001 <jatinoco@espe.edu.ec>
2024-02-27 14:26:16 -0600

    Paginador de Resultados
    La mejora adicional fue agregar un paginador de resultados

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,37 +1,62 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import "../App.css";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(
-          `https://api.github.com/users/${username}/repos`
+          `https://api.github.com/users/${username}/repos?page=${page}&per_page=5`
         );
         const sortedRepos = response.data.sort((a, b) => b.size - a.size);
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        setRepos(sortedRepos);
       } catch (error) {
         console.error("Error fetching repos:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, [username]);
+  }, [username, page]);
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
   return (
     <div>
       <h2>Top 5 repositorios con más participación de {username}</h2>
-      <ul>
-        {repos.map((repo) => (
-          <li key={repo.id}>
-            {repo.name} - Tamaño: {repo.size}
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <ul>
+            {repos.map((repo) => (
+              <li key={repo.id}>
+                {repo.name} - Tamaño: {repo.size}
+              </li>
+            ))}
+          </ul>
+          <div>
+            <button className="prevNextButton" onClick={handlePrevPage}>Previous</button>
+            <button className="prevNextButton" onClick={handleNextPage}>Next</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
