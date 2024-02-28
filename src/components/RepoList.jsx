f918784470e77e831ed04ad8f6dc04c4d3b72a76
msbgod <mbsocasi@espe.edu.ec>
2024-02-27 21:06:43 -0500

    paginacion

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import { Button, Pagination, Typography, Box } from "@mui/material";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10; // Número de repositorios por página
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,19 +15,25 @@ const RepoList = ({ username }) => {
           `https://api.github.com/users/${username}/repos`
         );
         const sortedRepos = response.data.sort((a, b) => b.size - a.size);
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        const startIndex = (currentPage - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+        const reposForPage = sortedRepos.slice(startIndex, endIndex);
+        setRepos(reposForPage);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
     };
 
     fetchData();
-  }, [username]);
+  }, [username, currentPage]);
+
+  const handlePageChange = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div>
-      <h2>Top 5 repositorios con más participación de {username}</h2>
+      <Typography variant="h6">Top 5 repositorios con más participación de {username}</Typography>
       <ul>
         {repos.map((repo) => (
           <li key={repo.id}>
@@ -32,6 +41,14 @@ const RepoList = ({ username }) => {
           </li>
         ))}
       </ul>
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Pagination
+          count={Math.ceil(repos.length / pageSize)}
+          page={currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
+      </Box>
     </div>
   );
 };
